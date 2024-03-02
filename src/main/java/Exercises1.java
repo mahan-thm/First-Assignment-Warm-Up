a9aa2047f2b91319369a1a9d0f016c346eaa3745
moon <m.tahmasbi1383@gmail.com>
2024-03-02 22:52:59 +0330

    final

--- src/main/java/Exercises1.java
+++ src/main/java/Exercises1.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class Exercises1 {
-    /*
-    implement a function that returns factorial of given n
-     */
-    public long factorial(int n) {
-        // TODO
-        return 0;
+    public static long factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n ; i++)
+        {
+            result *= i;
+        }
+        return result;
     }
 
 
@@ -12,14 +15,24 @@ public class Exercises1 {
     implement a function that return nth number of fibonacci series
     the series -> 1, 1, 2, 3, 5, 8, ...
     */
-    public long fibonacci(int n) {
-        // TODO
-        return 0;
+    public static long fibonacci(int n)
+    {
+    long first = 1;
+    long second = 1;
+    long sum = 1;
+
+    for (int i = 3; i <= n; i++)
+    {
+        sum = first + second;
+        first = second;
+        second = sum;
+    }
+        return sum;
     }
 
 
     /*
-    implement a function that returns a triangle for example:
+    implement a function that returns a triange for example:
     row = 5
 
     *
@@ -29,14 +42,28 @@ public class Exercises1 {
     *****
 
      */
-    public char[][] generateTriangle(int rows) {
-        // TODO
+    public static char[][] generateTriangle(int rows) {
+        for(int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
         return null;
     }
 
 
     public static void main(String[] args) {
-        // test your code here!
+        Scanner input = new Scanner(System.in);
+//        int n = input.nextInt();
+//        System.out.println(factorial(n));
+//        int m = input.nextInt();
+//        System.out.println(fibonacci(m));
+            int o = input.nextInt();
+            generateTriangle(o);
+
+
+
     }
 
 }
